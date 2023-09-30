338a262517a018c0578e366390b332b9ef99fc04
Sourav Das <iamsouravdaswade420@gmail.com>
2023-10-01 01:03:43 +0530

    locked

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,8 @@
 # exit on error
 set -o errexit
 
-bundle lock --add-platform x86_64-linux
+# bundle lock --add-platform x86_64-linux   
+
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
